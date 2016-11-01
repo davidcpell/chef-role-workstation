fd225668c98def63da1059969a631cc1efb998b7
David Pell <davidcpell@gmail.com>
2016-11-01 19:30:46 -0400

    set up dependencies

--- recipes/default.rb
+++ recipes/default.rb
@@ -4,9 +4,14 @@
 #
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 
+include_recipe 'chef-sugar'
+
+include_recipe 'apt' if ubuntu?
+
 include_recipe 'ws_users'
 include_recipe 'ws_zsh'
 include_recipe 'ws_dotfiles'
 include_recipe 'ws_vim'
 include_recipe 'ws_vim::pathogen'
+include_recipe 'ws_ruby'
 include_recipe 'the_silver_searcher'
